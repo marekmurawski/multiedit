2e3a40aa76915e3d906bf4072020b8fb5283111f
marekmurawski <marek.murawski@gmail.com>
2012-06-28 20:47:00 +0200

    I18n

--- i18n/pl-message.php
+++ i18n/pl-message.php
@@ -2,9 +2,28 @@
 
     /**
      * Polish file for plugin multiedit
+     *
+     * @package Plugins
+     * @subpackage multiedit
+     *
+     * @author Marek Murawski <http://marekmurawski.pl>
+     * @version Wolf 0.7.5
+     * @version MultiEdit 0.0.5
      */
 
     return array(
-
+	'Hide item' => '',
+	'Ascending' => '',
+	'Descending' => '',
+	'Breadcrumb' => '',
+	'Refresh item' => '',
+	'Copy breadcrumb from title' => '',
+	'Make slug from title' => '',
+	'Messages' => '',
+	'MultiEdit Pages' => '',
+	'Edit in default editor' => '',
+	'Each field is updated upon leaving it. Changing <em>"title", "breadcrumb", "slug", "keywords", "description" and page parts</em> will change page "updated on" time.' => '',
+	'Here you can quickly edit multiple pages at once. The list of pages consists of the selected root page and children of it.' => '',
+	'Provides convenient interface to quickly edit multiple pages metadata.' => '',
+	'You can also edit page parts but only those <strong>without filter</strong> applied. Page parts with filters are listed below each page.' => '',
     );
-
