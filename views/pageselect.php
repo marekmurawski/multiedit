60589bf7e5e9a826b62a4ca698856f43ee3a7a57
marekmurawski <marek.murawski@gmail.com>
2012-06-29 09:24:51 +0200

    I18n

--- views/pageselect.php
+++ views/pageselect.php
@@ -3,15 +3,16 @@
 if (!defined('IN_CMS')) { exit(); }
 ?>
 
-<label for="rootpage">Show subpages of: </label>
+<label for="rootpage"><?php echo __('Show subpages of')?>: </label>
 <select name="rootpage" id="multiedit-pageslist" class="multiedit-items-select">
 			<option value="1" selected><?php echo $rootPage->title; ?></option>
 			<?php foreach($pagesList as $k): ?>
 			<option value="<?php echo $k['id'] ?>"><?php echo $k['label'] . ' (' . $k['count'] . ')' ?></option>
 			<?php endforeach; ?>
 </select>
-<label for="sorting"> sort by </label>
+<label for="sorting"> <?php echo __('sort by')?> </label>
 <select name="sorting" id="multiedit-pageslist-sorting" class="multiedit-items-select">
+			<option value="id" selected><?php echo __('ID'); ?></option>	
 			<option value="title"><?php echo __('Title'); ?></option>
 			<option value="breadcrumb"><?php echo __('Breadcrumb'); ?></option>
 			<option value="slug"><?php echo __('Slug'); ?></option>
@@ -20,13 +21,11 @@ if (!defined('IN_CMS')) { exit(); }
 			<option value="created_on"><?php echo __('Created on'); ?></option>
 			<option value="published_on"><?php echo __('Published on'); ?></option>
 			<option value="valid_until"><?php echo __('Valid until'); ?></option>	
-			<option value="id" selected><?php echo __('ID'); ?></option>	
 </select>
-<label for="order"> ordered </label>
 <select name="order" id="multiedit-pageslist-order" class="multiedit-items-select">
 			<option value="asc"><?php echo __('Ascending'); ?></option>
 			<option value="desc"><?php echo __('Descending'); ?></option>
 </select>
-<label for="order"> Show page parts </label>
+<label for="order"> <?php echo __('Show page parts'); ?> </label>
 <input type="checkbox" class="multiedit-items-select" name="showpageparts" id="showpageparts" value="1" checked="1"/>
 <img id="reload-list" src="<?php echo PLUGINS_URI.'multiedit/icons/arrow-circle-135-left.png'; ?>"/>
