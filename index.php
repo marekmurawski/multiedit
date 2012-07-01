9e305b139a6c0623393f9bf557e8934f3eadb276
marekmurawski <marek.murawski@gmail.com>
2012-07-01 13:32:44 +0200

    update url, version change, info

--- index.php
+++ index.php
@@ -2,24 +2,31 @@
 /*
  * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
  * Copyright (C) 2008-2010 Martijn van der Kleijn <martijn.niji@gmail.com>
+ * 
+ * MultiEdit Plugin for Wolf CMS
+ * Provides convenient interface to quickly edit multiple pages metadata.
+ *  
+ * @package Plugins
+ * @subpackage multiedit
  *
- * This file is part of Wolf CMS. Wolf CMS is licensed under the GNU GPLv3 license.
- * Please see license.txt for the full license text.
+ * @author Marek Murawski <http://marekmurawski.pl>
+ * @copyright Marek Murawski, 2012
+ * @license http://www.gnu.org/licenses/gpl.html GPLv3 license
  */
 
 /* Security measure */
 if (!defined('IN_CMS')) { exit(); }
 
 Plugin::setInfos(array(
-    'id'          => 'multiedit',
-    'title'       => 'MultiEdit',
-    'description' => __('Provides convenient interface to quickly edit multiple pages metadata.'),
-    'version'     => '0.0.6',
-   	'license'     => 'GPL',
-	'author'      => 'Marek Murawski',
-    'website'     => 'http://marekmurawski.pl/',
-    //'update_url'  => 'http://www.wolfcms.org/plugin-versions.xml',
-    'require_wolf_version' => '0.7.3'
+		'id'          => 'multiedit',
+		'title'       => 'MultiEdit',
+		'description' => __('Provides convenient interface to quickly edit multiple pages metadata.'),
+		'version'     => '0.0.7',
+			'license'     => 'GPL',
+			'author'      => 'Marek Murawski',
+		'website'     => 'http://marekmurawski.pl/',
+		'update_url'  => 'http://marekmurawski.pl/static/wolfplugins/plugin-versions.xml',
+		'require_wolf_version' => '0.7.3' // 0.7.5SP-1 fix -> downgrading requirement to 0.7.3
 ));
 
 Plugin::addController('multiedit', 'MultiEdit', 'administrator');
