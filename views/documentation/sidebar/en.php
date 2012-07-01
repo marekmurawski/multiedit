cefd4622edd4a86406962144ccf65eddea62c3cb
marekmurawski <marek.murawski@gmail.com>
2012-07-01 13:35:57 +0200

    I18n documentation in files

---
+++ views/documentation/sidebar/en.php
@@ -0,0 +1,14 @@
+<?php
+if (!defined('IN_CMS')) {
+	exit();
+}
+?>
+<p>
+Here you can quickly edit multiple pages at once. The list of pages consists of the selected root page and children of it.
+</p>
+<p>
+Each field is updated upon leaving it. Changing <em>"title", "breadcrumb", "slug", "keywords", "description" and page parts</em> will change page "updated on" time.
+</p>
+<p>
+You can also edit page parts but only those <strong>without filter</strong> applied. Page parts with filters are listed below each page.
+</p>
