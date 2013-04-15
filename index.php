6ca50720e925ca5b09027dafa7a8290bc1081901
marekmurawski <marek.murawski@gmail.com>
2013-04-15 09:20:43 +0200

    v0.3.1

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ Plugin::setInfos(array(
             'id'                   => 'multiedit',
             'title'                => 'MultiEdit',
             'description'          => __('Provides convenient interface to quickly edit multiple pages metadata.'),
-            'version'              => '0.3.0',
+            'version'              => '0.3.1',
             'license'              => 'GPL',
             'author'               => 'Marek Murawski',
             'website'              => 'http://marekmurawski.pl/',
